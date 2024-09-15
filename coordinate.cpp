1bf7ed085f7f8bd0a92eff362434d619a2dcbc73
PrabeshNepal <161407498+PrabeshNepal@users.noreply.github.com>
2024-09-15 22:46:18 +1000

    Create coordinate.cpp
    coordinate cpp

---
+++ coordinate.cpp
@@ -0,0 +1,15 @@
+#include "Coordinate.h"
+
+
+/*********** Declaration*******
+I hereby certify that no part of this assignment has been copied from
+any other student’s work or from any other source. No part of the
+code has been written/produced for me by another person or copied
+from any other source.
+I hold a copy of this assignment that I can produce if the original
+is lost or damaged.
+**************************/
+
+// initially sets coordinate to 0,0 however it is over ridden int he NBTicTacToe constructior
+int x = 0;
+int y = 0;
