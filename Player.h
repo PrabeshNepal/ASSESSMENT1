455f5833df45bc72e78902ba25bc8329fd26d759
PrabeshNepal <161407498+PrabeshNepal@users.noreply.github.com>
2024-09-15 22:49:24 +1000

    Create Player.h

---
+++ Player.h
@@ -0,0 +1,15 @@
+#ifndef PLAYER_H_
+#define PLAYER_H_
+
+
+
+// Forward declaration of classes
+// This class isnt implemented in the NBtictactoe as there were errors
+
+class Player
+{
+public: 
+	//NBTicTacToe nb;
+};
+
+#endif /* PLAYER_H_ */
